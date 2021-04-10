34ef46fc88f69445c07f16b32aca9a5b76c83600
ClancyGilroy <78936857+TinyVail@users.noreply.github.com>
2021-04-09 20:13:18 -0500

    put in search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 const key = "441ab10a773a3174b04e9e95a884b2bc";
 
+let visitedCities = [];
+
+
 
 function getWeatherOfCity(nameCity) {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${nameCity}&appid=${key}&units=imperial`;
@@ -13,9 +16,10 @@ function getWeatherOfCity(nameCity) {
             fetch(url)
                 .then((response) => {
                     return response.json();
-                }).then((data) => {
-                    console.log(data);
-                    drawWeatherOfCity(data);
+                }).then((weatherData) => {
+                    weatherData.cityName = nameCity;
+                    console.log(weatherData);
+                    drawWeatherOfCity(weatherData);
                 })
         });
 }
@@ -36,15 +40,43 @@ function drawWeatherOfCity(weatherData) {
     `;
 
     //date weather conditions temperature and humidity
-    for (let i=0; i<5; i++){
-        const currentDate = new Date(weatherData.daily.dt * 1000);
-        const currentTemp = weatherData.daily.temp;
-        const currentHumidity = weatherData.daily.humidity;
-        const currentWindspeed = weatherData.daily.windSpeed;
-        const weatherIcon = weatherData.daily.weather.icon;
-    }
-    
+    for (let i = 0; i < 5; i++) {
+        const currentDate = new Date(weatherData.daily[i].dt * 1000);
+        const dateOptions = { month: "long", day: "numeric", weekday: "long" };
 
+        const currentTemp = weatherData.daily[i].temp.day;
+        const currentHumidity = weatherData.daily[i].humidity;
+        const currentWindspeed = weatherData.daily[i].wind_speed;
+        const weatherIcon = weatherData.daily[i].weather[0].icon;
+        document.getElementById("weatherReturned").innerHTML += `
+            <div class="col">
+                <div class="card">
+                    <div class="card-body">
+                        ${currentDate.toLocaleDateString("en", dateOptions)}
+                        ${currentTemp} ℉
+                        ${currentHumidity} %
+                        ${currentWindspeed} MPH
+                        <img src="http://openweathermap.org/img/wn/${weatherIcon}@2x.png"></img>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+
+    addToVisitedCities(weatherData.cityName);
+    document.getElementById("citySearchHistory").innerHTML = `<ul class="list-group list-group-flush">`;
+    for (let i = 0; i < Math.min(8, visitedCities.length); i++) {
+        document.getElementById("citySearchHistory").innerHTML += ` <li class="list-group-item"> ${visitedCities[i]}</li>`;
+    }
+    document.getElementById("citySearchHistory").innerHTML += `</ul>`;
+
+}
+
+function addToVisitedCities(cityName) {
+    visitedCities = visitedCities.filter((visitedCity) => {
+        return cityName != visitedCity;
+    });
+    visitedCities = [cityName, ...visitedCities];
 }
 
 /*
